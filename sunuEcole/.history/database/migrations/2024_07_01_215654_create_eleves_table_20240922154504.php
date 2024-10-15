1b91bf04c4f1c30690c660367c36b7c97c4e8219
khadyba <bak92147@gmail.com>
2024-10-15 11:57:54 +0200

    affichage des éleves inscrit en fonction du type etablissement

---
+++ sunuEcole/.history/database/migrations/2024_07_01_215654_create_eleves_table_20240922154504.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eleves', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenoms');
+            $table->string('adresse');
+            $table->string('non_de_votre_tuteur');
+            $table->string('email_tuteur');
+            $table->date('dateDeNaissance');
+            $table->unsignedBigInteger('classe_id')->nullable();
+            $table->boolean('is_completed')->default(false); 
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('classe_id')->references('id')->on('classes')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('parents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eleves');
+    }
+};
---
+++ sunuEcole/.history/database/migrations/2024_07_01_215654_create_eleves_table_20241015103315.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eleves', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenoms');
+            $table->string('adresse');
+            $table->string('non_de_votre_tuteur');
+            $table->string('email_tuteur');
+            $table->date('dateDeNaissance');
+            $table->unsignedBigInteger('classe_id')->nullable();
+            $table->boolean('is_completed')->default(false); 
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('classe_id')->references('id')->on('classes')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('parents');
+            $table->unsignedBigInteger('etablissement_id')->nullable(); // Ajoute la colonne etablissement_id
+            $table->foreign('etablissement_id')->references('id')->on('etablissements')->onDelete('set null'); // Assure la relation
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eleves');
+    }
+};
